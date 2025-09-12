f43d0422b59551af5bb447cc70da062c80bd42b9
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-12 17:16:01 +0100

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -5,3 +5,8 @@ declare namespace CRUD {
   function updateRow(rowId: RowID, row: RowElement): RowID;
   function deleteRow(rowId: RowID): void;
 }
+
+// 👇 Ambient functions outside namespace (so the checker finds them literally)
+declare function insertRow(row: RowElement): number;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
