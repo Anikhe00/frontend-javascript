878cd44cae4e1f4b4f2ad1dfeee3b86b5c4ce69e
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-12 11:11:25 +0100

    Update the return statement of the printTeacher function to return full firstName and lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -36,5 +36,5 @@ interface printTeacherFunction {
 }
 
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName[0]}. ${lastName}`;
+  return `${firstName}. ${lastName}`;
 }
