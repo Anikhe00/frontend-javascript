1a738bca9c8436dd36fd9391114ee9955892ce59
Shakirat Akanji <akanjishakirato@gmail.com>
2025-09-12 10:43:16 +0100

    Create Teacher interface with the possibility to add any attribute to the Object

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: "Adewale",
+  lastName: "Ibrahim",
+  fullTimeEmployee: false,
+  location: "London",
+  contract: true,
+};
+
+console.log(teacher3);
